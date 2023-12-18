c5e5a59424c6f5710afd6bb4c9489943625ebbda
Muhammad Sony Setiawan <132990489+MuhammadSonySetiawan@users.noreply.github.com>
2023-12-19 02:42:59 +0700

    [UPDATE] Add padding to the footer component

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import UilLinkedin from "@iconscout/react-unicons/icons/uil-linkedin";
 
 function Footer() {
   return (
-    <div className="bg-[#007B88] w-full h-20 flex justify-around items-center mt-3">
+    <div className="bg-[#007B88] w-full h-20 flex justify-around items-center p-3 mt-3">
       <span className="text-[#85E6C5]">
         &#169;2023 Hidayah Al Quran, All Rights Reserved.{" "}
       </span>
